39347ffb748b487a896963cf7902dd685646a4f4
robsonsilv4 <robsonsilv410@gmail.com>
2020-01-16 05:48:58 -0300

    Projeto inicial

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+
+export default function App() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
