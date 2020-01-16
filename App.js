9afb93b0d34fa9fd5593960bbff8891fe99c75c6
robsonsilv4 <robsonsilv410@gmail.com>
2020-01-16 06:19:47 -0300

    Header e estado inicial

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 
-import React from 'react';
+import Header from './components/Header';
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Estudar CSS' },
+    { id: 2, text: 'Estudar FlexBox' },
+    { id: 3, text: 'Estudar React' },
+    { id: 4, text: 'Estudar React Native' },
+    { id: 5, text: 'Estudar Redux' },
+  ]);
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        {/* form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            keyExtractor={todo => todo.id}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -11,4 +34,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  content: {
+    padding: 40,
+  },
+  list: { marginTop: 20 },
 });
